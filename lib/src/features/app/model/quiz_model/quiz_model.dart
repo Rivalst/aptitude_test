b3f497b941a300539e063203c243a3e8524b159e
bohdan54844460 <bohdan@horda.dev>
2024-04-01 12:50:15 +0300

    Added quiz data
    Created quiz data class for local usage
    
    Also created quiz repo

---
+++ lib/src/features/app/model/quiz_model/quiz_model.dart
@@ -0,0 +1,15 @@
+import 'package:aptitude_test/src/features/app/model/quiz_option_model/quiz_option_model.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'quiz_model.freezed.dart';
+part 'quiz_model.g.dart';
+
+@freezed
+class Quiz with _$Quiz {
+  const factory Quiz({
+    required String question,
+    required List<QuizAnswer> answers,
+  }) = _Quiz;
+
+  factory Quiz.fromJson(Map<String, Object?> json) => _$QuizFromJson(json);
+}
